19791bf43c42073dd77e650e273131015ba94c3c
Anatoli Makarevich <makaroni4@gmail.com>
2023-01-14 19:01:18 +0100

    Remove header

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -5,7 +5,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">SQL Habit – Bindle dataset</header>
       <Flow />
     </div>
   );
