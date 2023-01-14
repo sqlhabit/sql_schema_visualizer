555da8f44de05a0f0a450baf341f68fbc03ce327
Anatoli Makarevich <makaroni4@gmail.com>
2023-01-14 19:06:03 +0100

    Add SQL Habit logo

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,10 +1,14 @@
 import Flow from '../Flow';
+import SQLHabitLogo from "./SQLHabitLogo";
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div className="App__logo">
+        <SQLHabitLogo/>
+      </div>
       <Flow />
     </div>
   );
