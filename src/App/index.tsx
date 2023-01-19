980e71ef4717020e9583b45b112b352d1e1a4835
Anatoli Makarevich <makaroni4@gmail.com>
2023-01-19 19:59:41 +0100

    Logo link

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,14 +1,18 @@
-import Flow from '../Flow';
+import Flow from "../Flow";
 import SQLHabitLogo from "./SQLHabitLogo";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <div className="App__logo">
+      <a
+        href="https://www.sqlhabit.com"
+        target="_blank"
+        rel="noreferrer"
+        className="App__logo">
         <SQLHabitLogo/>
-      </div>
+      </a>
       <Flow />
     </div>
   );
