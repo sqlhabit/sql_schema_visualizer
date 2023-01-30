1add89606862b9fe724a1c552d8d5d4dc297e955
Anatoli Makarevich <makaroni4@gmail.com>
2023-01-30 22:18:35 +0100

    Store database config files in one folder

--- src/Visualizer/helpers/loadTables.ts
+++ src/Visualizer/helpers/loadTables.ts
@@ -1,19 +1,19 @@
 import { TableConfig } from "../types";
 
-import accountsTable from "../../config/tables/accounts.json";
-import adjustCallbacksTable from "../../config/tables/adjust_callbacks.json";
-import booksUsersTable from "../../config/tables/books_users.json";
-import booksTable from "../../config/tables/books.json";
-import devicesTable from "../../config/tables/devices.json";
-import helpersDatesTable from "../../config/tables/helpers_dates.json";
-import marketingSpendsTable from "../../config/tables/marketing_spends.json";
-import mobileAnalyticsEventsTable from "../../config/tables/mobile_analytics_events.json";
-import productsTable from "../../config/tables/products.json";
-import profilesTable from "../../config/tables/profiles.json";
-import purchasesTable from "../../config/tables/purchases.json";
-import usersTable from "../../config/tables/users.json";
-import webAnalyticsEventsTable from "../../config/tables/web_analytics_events.json";
-import webAnalyticsPageviewsTable from "../../config/tables/web_analytics_pageviews.json";
+import accountsTable from "../../config/databases/bindle/tables/accounts.json";
+import adjustCallbacksTable from "../../config/databases/bindle/tables/adjust_callbacks.json";
+import booksUsersTable from "../../config/databases/bindle/tables/books_users.json";
+import booksTable from "../../config/databases/bindle/tables/books.json";
+import devicesTable from "../../config/databases/bindle/tables/devices.json";
+import helpersDatesTable from "../../config/databases/bindle/tables/helpers_dates.json";
+import marketingSpendsTable from "../../config/databases/bindle/tables/marketing_spends.json";
+import mobileAnalyticsEventsTable from "../../config/databases/bindle/tables/mobile_analytics_events.json";
+import productsTable from "../../config/databases/bindle/tables/products.json";
+import profilesTable from "../../config/databases/bindle/tables/profiles.json";
+import purchasesTable from "../../config/databases/bindle/tables/purchases.json";
+import usersTable from "../../config/databases/bindle/tables/users.json";
+import webAnalyticsEventsTable from "../../config/databases/bindle/tables/web_analytics_events.json";
+import webAnalyticsPageviewsTable from "../../config/databases/bindle/tables/web_analytics_pageviews.json";
 
 const tables: TableConfig[] = [
   accountsTable,
