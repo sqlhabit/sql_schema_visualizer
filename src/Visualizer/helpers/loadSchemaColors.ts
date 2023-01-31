b9fd908c9b7fdcbd273db2c4d114e5fae8d5f4e3
Anatoli Makarevich <makaroni4@gmail.com>
2023-01-31 20:07:08 +0100

    Dropped obsolete helpers

--- src/Visualizer/helpers/loadSchemaColors.ts
+++
@@ -1,6 +0,0 @@
-import { SchemaColors } from "../types/SchemaColors";
-import schemaColors from "../../config/databases/bindle/schemaColors.json";
-
-export const loadSchemaColors = (): SchemaColors => {
-  return (schemaColors as SchemaColors);
-};
--- src/Visualizer/helpers/loadTablePositions.ts
+++
@@ -1,6 +0,0 @@
-import { TablePositions } from "../types";
-import tablePositions from "../../config/databases/bindle/tablePositions.json";
-
-export const loadTablePositions = (): TablePositions => {
-  return tablePositions;
-};
