c24963d2a468bc9ab206b8c4b5cd0016186aa28a
Anatoli Makarevich <makaroni4@gmail.com>
2023-01-31 21:12:28 +0100

    Fix edges import

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,11 +1,13 @@
 import { Databases } from "../Visualizer/types";
 import { loadDatabaseConfig } from "../Visualizer/helpers";
-import databaseName from "./databases.json";
+import databaseNames from "./databases.json";
 
 const databases: Databases = {};
 
-databaseName.forEach(async databaseName => {
-  databases[databaseName] = await loadDatabaseConfig(databaseName);
+databaseNames.forEach(async databaseName => {
+  const databaseConfig = await loadDatabaseConfig(databaseName);
+
+  databases[databaseName] = databaseConfig;
 });
 
 export default databases;
