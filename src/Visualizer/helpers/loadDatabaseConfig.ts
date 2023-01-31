0bec624acaf8622fc03a5fc28d7e8dae62180ead
Anatoli Makarevich <makaroni4@gmail.com>
2023-01-31 20:05:03 +0100

    Dynamically load DB configs

--- src/Visualizer/helpers/loadDatabaseConfig.ts
+++ src/Visualizer/helpers/loadDatabaseConfig.ts
@@ -1,6 +1,28 @@
-// import { Databases } from "../types/Database";
-// import edgeConfigs from "../../config/databases/bindle/edges.json";
+import { EdgeConfig, SchemaColors, TableConfig, TablePositions } from "../types";
+import { fullTableName } from "./fullTableName";
 
-export const loadDatabaseConfig = (): Boolean => {
-  return true;
+export const loadDatabaseConfig = async (databaseName: string) => {
+  const edgeConfigs = (await import(`../../config/databases/${databaseName}/edges.json`)).default as EdgeConfig[];
+  const tablePositions = (await import(`../../config/databases/${databaseName}/tablePositions.json`)).default as TablePositions;
+  const schemaColors = (await import(`../../config/databases/${databaseName}/schemaColors.json`)).default as SchemaColors;
+  const tables = (await import(`../../config/databases/${databaseName}/tables`)).default as TableConfig[];
+
+  edgeConfigs.forEach((edgeConfig: EdgeConfig) => {
+    const sourceTableName = fullTableName(edgeConfig.source);
+    const targetTableName = fullTableName(edgeConfig.target);
+
+    edgeConfig.source = sourceTableName;
+    edgeConfig.target = targetTableName;
+  });
+
+  tables.forEach(table => {
+    table.schemaColor = schemaColors[table.schema || "DEFAULT"];
+  });
+
+  return {
+    tables,
+    tablePositions,
+    edgeConfigs,
+    schemaColors
+  };
 }
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,34 +1,11 @@
-import { fullTableName } from "../Visualizer/helpers/fullTableName";
-import { EdgeConfig, SchemaColors, TableConfig, TablePositions } from "../Visualizer/types";
 import { Databases } from "../Visualizer/types/Database";
+import { loadDatabaseConfig } from "../Visualizer/helpers/loadDatabaseConfig";
+import databaseName from "./databases.json";
 
 const databases: Databases = {};
-const DATABASE_NAMES = ["bindle"];
 
-DATABASE_NAMES.forEach(async databaseName => {
-  const edgeConfigs = (await import("./databases/bindle/edges.json")).default as EdgeConfig[];
-  const tablePositions = (await import("./databases/bindle/tablePositions.json")).default as TablePositions;
-  const schemaColors = (await import("./databases/bindle/schemaColors.json")).default as SchemaColors;
-  const tables = (await import("./databases/bindle/tables")).default as TableConfig[];
-
-  edgeConfigs.forEach((edgeConfig: EdgeConfig) => {
-    const sourceTableName = fullTableName(edgeConfig.source);
-    const targetTableName = fullTableName(edgeConfig.target);
-
-    edgeConfig.source = sourceTableName;
-    edgeConfig.target = targetTableName;
-  });
-
-  tables.forEach(table => {
-    table.schemaColor = schemaColors[table.schema || "DEFAULT"];
-  });
-
-  databases[databaseName] = {
-    tables,
-    tablePositions,
-    edgeConfigs,
-    schemaColors
-  }
+databaseName.forEach(async databaseName => {
+  databases[databaseName] = await loadDatabaseConfig(databaseName);
 });
 
 export default databases;
