dab835b2eb6cbf4cbc68c0b35276e4bb75bbe0f4
Anatoli Makarevich <makaroni4@gmail.com>
2023-02-04 20:37:24 +0100

    Extract calculation of edges into function

--- src/Visualizer/index.tsx
+++ src/Visualizer/index.tsx
@@ -43,6 +43,41 @@ interface FlowProps {
   database?: string;
 }
 
+interface CalculateEdgesOptions {
+  nodes: Node[];
+  databaseConfig: Database;
+}
+
+const calculateEdges = ({ nodes, databaseConfig }: CalculateEdgesOptions) => {
+  const initialEdges: Edge[] = [];
+
+  databaseConfig.edgeConfigs.forEach((edgeConfig: EdgeConfig) => {
+    const sourceNode = nodes.find((node: Node) => node.id === edgeConfig.source);
+    const targetNode = nodes.find((node: Node) => node.id === edgeConfig.target);
+
+    if(sourceNode && targetNode) {
+      const sourcePosition = edgeConfig.sourcePosition || calculateSourcePosition(sourceNode.width as number, sourceNode!.position.x, targetNode.width as number, targetNode!.position.x);
+      const targetPosition = edgeConfig.targetPosition || calculateTargetPosition(sourceNode.width as number, sourceNode!.position.x, targetNode.width as number, targetNode!.position.x);
+
+      const sourceHandle = `${edgeConfig.sourceKey}-${sourcePosition}`;
+      const targetHandle = `${edgeConfig.targetKey}-${targetPosition}`;
+
+      initialEdges.push({
+        id: `${edgeConfig.source}-${edgeConfig.target}`,
+        source: edgeConfig.source,
+        target: edgeConfig.target,
+        sourceHandle,
+        targetHandle,
+        type: "smoothstep",
+        markerEnd: edgeMarkerName(edgeConfig, targetPosition),
+        className: edgeClassName(edgeConfig, targetPosition)
+      });
+    }
+  });
+
+  return initialEdges;
+};
+
 const Flow: React.FC<FlowProps> = (props: FlowProps) => {
   const databaseSlug = (props.database as string);
 
@@ -71,29 +106,8 @@ const Flow: React.FC<FlowProps> = (props: FlowProps) => {
     console.log("--> onInit");
 
     const nodes = instance.getNodes();
-    const initialEdges: Edge[] = [];
 
-    databaseConfig.edgeConfigs.forEach((edgeConfig: EdgeConfig) => {
-      const sourceNode = nodes.find((node: Node) => node.id === edgeConfig.source);
-      const targetNode = nodes.find((node: Node) => node.id === edgeConfig.target);
-
-      const sourcePosition = edgeConfig.sourcePosition || calculateSourcePosition(sourceNode.width, sourceNode!.position.x, targetNode.width, targetNode!.position.x);
-      const targetPosition = edgeConfig.targetPosition || calculateTargetPosition(sourceNode.width, sourceNode!.position.x, targetNode.width, targetNode!.position.x);
-
-      const sourceHandle = `${edgeConfig.sourceKey}-${sourcePosition}`;
-      const targetHandle = `${edgeConfig.targetKey}-${targetPosition}`;
-
-      initialEdges.push({
-        id: `${edgeConfig.source}-${edgeConfig.target}`,
-        source: edgeConfig.source,
-        target: edgeConfig.target,
-        sourceHandle,
-        targetHandle,
-        type: "smoothstep",
-        markerEnd: edgeMarkerName(edgeConfig, targetPosition),
-        className: edgeClassName(edgeConfig, targetPosition)
-      });
-    });
+    const initialEdges = calculateEdges({ nodes, databaseConfig })
 
     setEdges(eds => initialEdges);
 
