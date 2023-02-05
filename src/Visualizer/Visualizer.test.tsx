7a8ffd234980d8518342166899b09c100011f9a4
Anatoli Makarevich <makaroni4@gmail.com>
2023-02-05 22:40:33 +0100

    Fix Visualizer specs

--- src/Visualizer/Visualizer.test.tsx
+++ src/Visualizer/Visualizer.test.tsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render, waitFor } from "@testing-library/react";
 import Visualizer from ".";
 
 const wrapperStyle = { width: 1200, height: 800 };
@@ -10,16 +10,17 @@ test("renders nodes and edges", async () => {
     </div>
   );
 
-  console.log(container.innerHTML);
 
-  // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
-  const nodes = container.getElementsByClassName("react-flow__node");
-  // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
-  const edges = container.getElementsByClassName("react-flow__edge");
+  await waitFor(() => {
+    // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
+    const nodes = container.getElementsByClassName("react-flow__node");
+    expect(nodes.length).toBe(14);
 
-  expect(nodes.length).toBe(14);
+  });
 
-  // Edges are added onInit, so we know node widths
-  // and can properly calculate edge handle positions.
-  expect(edges.length).toBe(0);
+  await waitFor(() => {
+    // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
+    const edges = container.getElementsByClassName("react-flow__edge");
+    expect(edges.length).toBe(0);
+  });
 });
