5814b36914097277bebb841844c9b271d2344fee
Anatoli Makarevich <makaroni4@gmail.com>
2024-11-05 18:16:17 +0100

    Added mobile_analytics schema description for Live dataset

--- src/config/databases/live/tables.ts
+++ src/config/databases/live/tables.ts
@@ -4,12 +4,14 @@ import productsTable from "./tables/products.json";
 import purchasesTable from "./tables/purchases.json";
 import trialsTable from "./tables/trials.json";
 import usersTable from "./tables/users.json";
+import mobileAnalyticsEventsTable from "./tables/mobile_analytics_events.json";
 
 const tables: TableConfig[] = [
   productsTable,
   purchasesTable,
   trialsTable,
-  usersTable
+  usersTable,
+  mobileAnalyticsEventsTable
 ];
 
 export default tables;
