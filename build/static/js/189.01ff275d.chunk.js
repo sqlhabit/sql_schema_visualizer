63e0a86789628c641cbda8583a82ef5f438e9700
Anatoli Makarevich <makaroni4@gmail.com>
2025-11-22 23:54:24 +0100

    feat: added more bank job tables

---
+++ build/static/js/189.01ff275d.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksql_schema_visualizer=self.webpackChunksql_schema_visualizer||[]).push([[189],{4189:function(s){s.exports=JSON.parse('{"DEFAULT":"#91C4F2","bank":"#91C4F2","interpol":"#F4A261","sellmycar_com":"#8B5CF6","nyc_dot":"#EC4899"}')}}]);
--- src/config/databases/the-bank-job/tables.ts
+++ src/config/databases/the-bank-job/tables.ts
@@ -5,12 +5,15 @@ import vaultsTable from "./tables/vaults.json";
 import safeDepositBoxesTable from "./tables/safe_deposit_boxes.json";
 import eventLogTable from "./tables/event_log.json";
 import employeesTable from "./tables/employees.json";
+import authorizedVehiclesTable from "./tables/authorized_vehicles.json";
 import casesTable from "./tables/cases.json";
 import crimeTypesTable from "./tables/crime_types.json";
 import criminalsTable from "./tables/criminals.json";
 import criminalAliasesTable from "./tables/criminal_aliases.json";
 import casesCrimeTypesTable from "./tables/cases_crime_types.json";
 import casesCriminalsTable from "./tables/cases_criminals.json";
+import vehiclesTable from "./tables/vehicles.json";
+import roadCameraEventsTable from "./tables/road_camera_events.json";
 
 const tables: TableConfig[] = [
   accountsTable,
@@ -18,12 +21,15 @@ const tables: TableConfig[] = [
   safeDepositBoxesTable,
   eventLogTable,
   employeesTable,
+  authorizedVehiclesTable,
   casesTable,
   crimeTypesTable,
   criminalsTable,
   criminalAliasesTable,
   casesCrimeTypesTable,
-  casesCriminalsTable
+  casesCriminalsTable,
+  vehiclesTable,
+  roadCameraEventsTable
 ];
 
 export default tables;
